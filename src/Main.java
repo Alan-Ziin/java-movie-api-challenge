fde3d6741eb2f1103c187b1313024447e3d641f9
Alan Sales <alansales2018@gmail.com>
2026-09-03 22:20:56 -0300

    Dia 3 e 4: gerar página HTML com dados dos filmes
    - Adicionando uma segunda requisição na API (&i=) para buscar imdbRating (notas) por filme
    - Usando @JsonIgnoreProperties(ignoreUnknown=true) para lidar com campos extras que o JSON retornava nessa requisição nova
    - HTML dinamicamente concatenando cards (título, poster, nota) em loop
    - Escreve o HTML em arquivo com FileWriter (try-with-resources)
    
    - Me empolguei aqui, acabei fazendo 2 dias seguidos rs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main{
@@ -14,7 +18,7 @@ public class Main{
                 .uri(URI.create("http://www.omdbapi.com/?s=batman&apikey=85c1f01e"))
                 .GET()
                 .build();
-        try{
+        try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             // o segundo argumento pega os bytes da resposta e os traduz para uma String.
 
@@ -22,9 +26,43 @@ public class Main{
 
             MovieSearchResponse filmesListados = mapper.readValue(response.body(), MovieSearchResponse.class); // chama um método de
             // leitura, passando o JSON (texto) e dizendo pra qual classe ele deve converter, no meu caso Class MovieSearchResponse
+            List<Movie> ResultadoComRating = new ArrayList<>(); // lista nova e vazia, vai guardar os filmes "completos", já com nota
+            for (Movie tituloFilmes : filmesListados.search) { // percorre cada filme da busca que é: (&s=), um por vez
 
-            for (Movie tituloFilmes : filmesListados.search)
-                System.out.println(tituloFilmes.title);
+                HttpClient clientRating = HttpClient.newHttpClient(); // novo client, só pra essa segunda chamada por ID
+
+                HttpRequest requestRating = HttpRequest.newBuilder()
+                        // monta a URL de busca por ID (&i=), usando o imdbID do filme atual do loop
+                        .uri(URI.create("http://www.omdbapi.com/?i=" + tituloFilmes.getImdbID() + "&apikey=85c1f01e"))
+                        .GET()
+                        .build();
+                try {
+                    HttpResponse<String> responseRating = clientRating.send(requestRating, HttpResponse.BodyHandlers.ofString());
+                    // esse JSON de retorno é de UM filme só (não vem dentro de "Search": [...] como antes)
+
+                    Movie filmesListadosComRating = mapper.readValue(responseRating.body(), Movie.class);
+                    // deserializa direto pra Movie, já que o JSON aqui representa um filme único
+
+                    ResultadoComRating.add(filmesListadosComRating); // adicionando na lista nova
+
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            String htmlFilmes = "<html>" + "<head>" + "</head>" + "<body>";
+            for (Movie filme : ResultadoComRating){
+                htmlFilmes = htmlFilmes + "<div id='cardFilme'>"
+                        + "<h2>" + filme.getTitle() + "</h2>"
+                        + "<img src='" + filme.getPoster() + "' alt='poster do filme'>"
+                        + "<p>" + filme.getImdbRating() + "</p>"
+                        + "</div>";
+            }
+            htmlFilmes = htmlFilmes + "</body>" + "</html>";
+            try (FileWriter writer = new FileWriter("filmes.html")){
+                writer.write(htmlFilmes);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
         catch (IOException | InterruptedException e){
             e.printStackTrace();
--- src/Movie.java
+++ src/Movie.java
@@ -1,20 +1,55 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 // Utilizando o Jackson, biblioteca que converte objetos Java em strings JSON e strings JSON em objetos Java.
-
-public class Movie {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Movie  {
     @JsonProperty("Title") // JsonProperty Ela permite você ter um nome "bonito", para dizer pro Jackson
     // "essa variavel corresponde a esse campo específico do JSON: no meu caso title
-    public String title;
+    private String title;
+    @JsonProperty("Year") private String year;
+    @JsonProperty("imdbID") private String imdbID; // O Api já está devolvendo o nome imdbID
+    @JsonProperty("Type") private String type;
+    @JsonProperty("Poster") private String poster;
+    @JsonProperty("imdbRating")private String imdbRating;
 
-    @JsonProperty("Year")
-    public String year;
+    @JsonCreator // Metodo construtor do Jackson para "construir" o objeto do Movie
+    public Movie(
+        @JsonProperty("Title") String title,
+        @JsonProperty("Year") String year,
+        @JsonProperty("ImdbID") String imdbID,
+        @JsonProperty("Type") String type,
+        @JsonProperty("Poster") String poster,
+        @JsonProperty("imdbRating") String imdbRating
+        ){
+        this.title = title;
+        this.year = year;
+        this.imdbID = imdbID;
+        this.type = type;
+        this.poster = poster;
+        this.imdbRating = imdbRating;
+    }
+    public String getTitle() {
+        return title;
+    }
 
-    public String imdbID; // O Api já está devolvendo o nome imdbID
+    public String getYear(){
+        return year;
+    }
 
-    @JsonProperty("Type")
-    public String type;
+    public String getType(){
+        return type;
+    }
 
-    @JsonProperty("Poster")
-    public String poster;
+    public String getPoster(){
+        return poster;
+    }
+
+    public String getImdbID(){
+        return imdbID;
+    }
+    public String getImdbRating(){
+        return imdbRating;
+    }
 }
